2e133c4b441458715d3c16ef79bdad30e99a0d71
randsonbredley <randsonbredley@gmail.com>
2026-09-25 14:31:42 -0300

    Lidando com usuários imprevisíveis (Scanner)
    Construção da class GameHelper e da atividade 3.1: o usuário "engraçadinho" com a construção de uma Main simples chamando o método getUserInput que recebe a palavra batata

---
+++ Excecoes/src/TesteAtividade31.java
@@ -0,0 +1,11 @@
+
+public class TesteAtividade31 {
+	public static void main(String[] args) {
+		GameHelper helper = new GameHelper();
+		
+		int num = helper.getUserInput("Digite um número");
+		
+		System.out.println("Você digitou o número: " + num);
+	}
+
+}
